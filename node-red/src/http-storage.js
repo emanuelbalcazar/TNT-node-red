77072e7f36120358572b558b60ee2e260118909b
emanuel <emanuelbalcazar13@gmail.com>
2018-04-03 17:01:34 -0300

    Primera aproximacion base

---
+++ node-red/src/http-storage.js
@@ -0,0 +1,37 @@
+// https://nodered.org/docs/api/storage/methods/
+
+exports.init = function (settings) {
+
+};
+
+exports.getFlows = function () {
+
+};
+
+exports.saveFlows = function (flows) {
+
+};
+
+exports.getCredentials = function () {
+
+};
+
+exports.saveCredentials = function (credentials) {
+
+};
+
+exports.getSettings = function () {
+
+};
+
+exports.saveSettings = function (settings) {
+
+};
+
+exports.getSessions = function () {
+
+};
+
+exports.saveSessions = function (sessions) {
+
+};

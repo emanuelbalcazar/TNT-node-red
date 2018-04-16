67f1e63742babc42b11bf0c6553c9ec403ae241d
emanuel <emanuelbalcazar13@gmail.com>
2018-04-16 14:58:54 -0300

    Informacion del storage api en la ruta rest

--- storage-api/src/routes/routes.js
+++ storage-api/src/routes/routes.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const Flow = require('../models/flow');
 
+router.get('/info', (req, res) => {
+    res.send({ name: "storage api", version: 1.0 });
+});
+
 // get flows from database.
 router.get('/getFlows', (req, res) => {
     res.send({ data: "emanuel" });

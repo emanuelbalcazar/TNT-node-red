e7a117b6d0881a00065f3d49f6330d0d5b09e68d
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2018-05-04 15:47:29 -0300

    Aplicacion Node-Red incorporado en un servidor ExpressJS

---
+++ node-red/src/index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const express = require("express");
+const RED = require("node-red");
+
+// create an Express app
+const app = express();
+
+// add a simple route for static content served from 'public'
+app.use("/", express.static("public"));
+
+// create a server
+const server = http.createServer(app);
+
+// Create the settings object - see default settings.js file for other options
+var settings = {
+    httpAdminRoot:"/nodered",
+    httpNodeRoot: "/api",
+    httpRoot: "/nodered",
+    userDir:"~/.node-red",
+    storageModule: require("./http-storage"),
+    functionGlobalContext: { }    // enables global context
+};
+
+// initialise the runtime with a server and settings
+RED.init(server,settings);
+
+// serve the editor UI from /red
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+
+// serve the http nodes UI from /api
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(8200);
+
+// start the runtime
+RED.start();

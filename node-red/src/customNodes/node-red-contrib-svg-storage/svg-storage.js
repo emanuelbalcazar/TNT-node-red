f523463de77f87d3c07f25ea022d4d8891694a61
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2018-05-23 17:41:32 -0300

    Algunos acomodos de codigo

--- node-red/src/customNodes/node-red-contrib-svg-storage/svg-storage.js
+++ node-red/src/customNodes/node-red-contrib-svg-storage/svg-storage.js
@@ -3,19 +3,20 @@ module.exports = function (RED) {
     function SVGStorage(config) {
         RED.nodes.createNode(this, config);
         var node = this;
+        // get filename and content from svg-storage node.
         var filename = config.filename;
         var filecontent = config.content;
 
+        // check topic en msg.req.url
         node.on('input', function (msg) {
             if (msg.req.url === "/svg-list") {
                 msg.payload = [{ id: 1, name: filename }];
                 return node.send(msg);
             }
-
             msg.payload = filecontent;
             node.send(msg);
         });
     }
-    console.log('Registrando el nodo svg-storage...')
+    console.log('[svg storage] - Registrando el nodo svg-storage...')
     RED.nodes.registerType("svg-storage", SVGStorage);
 }

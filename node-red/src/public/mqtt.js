b45bed6964aa611a843daba3cd76a6997d67e8ea
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2018-06-01 17:13:01 -0300

    Intentando conectarme a mqtt desde el cliente js

--- node-red/src/public/mqtt.js
+++ node-red/src/public/mqtt.js
@@ -2,31 +2,57 @@
  * https://www.npmjs.com/package/web-mqtt-client
  */
 var client = new MqttClient({
-    host: 'some.domain.tld/mqtt',
-    port: 5678,
+    host: 'localhost',
+    port: 9001,
     will: {
-        topic: 'farewells',
+        topic: 'messages',
         payload: 'So long!',
     }
 });
 
-/* var sampleSVG = d3.select("#viz")
+console.log('[mqtt] - cliente creado', client);
+
+//client.connect();
+client.publish('messages', 'Hola', {}, () => {});
+
+
+client.on('connecting', () => {
+    console.log('[mqtt] - conectando...');
+});
+
+client.on('connect', () => {
+    console.log('[mqtt] - conectado');
+});
+
+client.on('disconnect', () => {
+    console.log('[mqtt] - desconectado');
+});
+
+client.on('offline', () => {
+    console.log('[mqtt] - desconectado, intente conectarse manualmente');
+});
+
+client.on('message', console.log.bind(console, 'MQTT message arrived: '));
+
+client.on('message', (topic, payload, details) => {
+    console.log('[mqtt] - [%s] - %s', topic, payload);
+});
+
+
+var sampleSVG = d3.select("#canvas")
     .append("svg")
     .attr("width", 100)
-    .attr("height", 100);    
-    
+    .attr("height", 100);
+
 sampleSVG.append("circle")
     .style("stroke", "gray")
     .style("fill", "white")
     .attr("r", 40)
     .attr("cx", 50)
     .attr("cy", 50)
-  .transition()
+    .transition()
     .delay(100)
-    .duration(1000)    
+    .duration(1000)
     .attr("r", 10)
     .attr("cx", 30)
-    .style("fill", "black"); */
-
-console.log('mqtt', client);
-console.log('d3', d3);
+    .style("fill", "black");

671c71fa4139322d72a8db341af5779956c50685
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2018-06-08 16:00:18 -0300

    Se agregan formas en el svg

--- node-red/src/public/mqtt.js
+++ node-red/src/public/mqtt.js
@@ -1,24 +1,17 @@
+// topic
+const TOPIC = '/TNT/#';
+
+// generate random client id.
 const clientId = parseInt(Math.random() * 100, 10);
-console.log('Creando ID de cliente:', clientId);
+console.log('[MQTT] - Creando ID de cliente:', clientId);
 
-var client = new Paho.MQTT.Client(window.location.host, 80, "/mqtt", clientId.toString());
-
-// se ejecuta cuando llega un mensaje.
-client.onMessageArrived = (message) => {
-    console.log('[MQTT] - Nuevo mensaje:', message);
-
-    d3.select("circle").style("fill", "red");
-    d3.select("circle").style("stroke", "blue");
-    d3.select("circle").attr("cx", 150)
-    d3.select("circle").attr("cy", 150)
-    d3.select("circle").attr("r", 80);
-}
+const client = new Paho.MQTT.Client(window.location.host, 80, "/mqtt", clientId.toString());
 
 client.connect({
     onSuccess: () => {
         console.log('[MQTT] - Me conecte');
-        //client.publish('/a', 'Hola soy el cliente TNT', 0, false);
-        client.subscribe('/test/#', {
+
+        client.subscribe(TOPIC, {
             onSuccess: () => {
                 console.log('[MQTT] - Me subscribi correctamente');
             },
@@ -32,21 +25,55 @@ client.connect({
     }
 });
 
+client.onMessageArrived = (message) => {
+    // ver el topico para saber QUE svg modificar (ej: circulo, cuadrado)
+    // extraer el payload y modificar los attr y styles que contenga.
+    console.log('[MQTT] - Llego un nuevo mensaje:', message);
+    d3.select("circle").style("fill", "red");
+    d3.select("circle").style("stroke", "blue");
+    d3.select("circle").attr("cx", 150)
+    d3.select("circle").attr("cy", 150)
+    d3.select("circle").attr("r", 80);
+}
 
-var sampleSVG = d3.select("#canvas")
-    .append("svg")
-    .attr("width", 300)
-    .attr("height", 300);
+const circleData = [
+    { "cx": 20, "cy": 20, "radius": 20, "color": "green" },
+    { "cx": 70, "cy": 70, "radius": 20, "color": "purple" }];
 
-sampleSVG.append("circle")
-    .style("stroke", "gray")
-    .style("fill", "white")
-    .attr("r", 40)
-    .attr("cx", 50)
-    .attr("cy", 50)
-    .transition()
-    .delay(100)
-    .duration(1000)
-    .attr("r", 10)
-    .attr("cx", 30)
-    .style("fill", "black");
+const rectangleData = [
+    { "rx": 110, "ry": 110, "height": 30, "width": 30, "color": "blue" },
+    { "rx": 160, "ry": 160, "height": 30, "width": 30, "color": "red" }];
+
+// container
+var svgContainer = d3.select("#canvas").append("svg")
+    .attr("width", 200)
+    .attr("height", 200);
+
+// circle group <g></g>
+var circleGroup = svgContainer.append("g");
+
+// create all circles
+var circles = circleGroup.selectAll("circle")
+    .data(circleData)
+    .enter()
+    .append("circle");
+
+// set attributes    
+var circleAttributes = circles
+    .attr("cx", function (d) { return d.cx; })
+    .attr("cy", function (d) { return d.cy; })
+    .attr("r", function (d) { return d.radius; })
+    .style("fill", function (d) { return d.color; });
+
+// create all rectangles
+var rectangles = svgContainer.selectAll("rect")
+    .data(rectangleData)
+    .enter()
+    .append("rect");
+
+var rectangleAttributes = rectangles
+    .attr("x", function (d) { return d.rx; })
+    .attr("y", function (d) { return d.ry; })
+    .attr("height", function (d) { return d.height; })
+    .attr("width", function (d) { return d.width; })
+    .style("fill", function (d) { return d.color; });
